3b7fef904ad9292ba1e81c855f2eee8ac66160db
glennbech <glenn.bech@gmail.com>
2021-11-08 22:53:10 +0100

    Ready version

---
+++ src/main/java/com/example/demo/BankExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class BankExceptionHandler {
+    private Logger logger = LoggerFactory.getLogger(BankExceptionHandler.class);
+
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public ResponseEntity<Object> exception(IllegalArgumentException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
